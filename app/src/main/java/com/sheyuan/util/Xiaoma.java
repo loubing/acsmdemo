969abb943198db08fd39ae866dfa7150e4a43b86
yingbinehao <loubing@acsm.cn>
2017-09-19 10:50:56 +0800

    新增日志工具[2C

---
+++ app/src/main/java/com/sheyuan/util/Xiaoma.java
@@ -0,0 +1,14 @@
+package com.sheyuan.util;
+
+import android.util.Log;
+
+/**
+ * Created by loub on 2017/9/19.
+ */
+
+public class Xiaoma {
+    
+    public static void log(String msg){
+        Log.e("xiaoma",msg);
+    }
+}
