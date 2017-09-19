2e3686e1b2fa626363610d8047bc28dec9384844
yingbinehao <loubing@acsm.cn>
2017-09-19 10:21:19 +0800

    acsmsdk
    init

---
+++ acsmdemo/src/main/java/com/sheyuan/acsmdemo/MyApp.java
@@ -0,0 +1,19 @@
+package com.sheyuan.acsmdemo;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by loub on 2017/9/19.
+ */
+
+public class MyApp extends Application {
+    
+    public static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext=this;
+    }
+}
